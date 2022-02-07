632d06a2d5b52bde989a956e2075e56c8d20f9bd
Adriano S Cesar <ascesar1@hotmail.com>
2022-02-07 19:09:48 -0300

    up

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,13 +1,13 @@
-function verificar() {
-    let senha = document.getElementById("inicio")
-    let res = document.getElementById("res") 
-    if (senha.value != "Billy") {
-        window.alert("Senha incorreta! Digite a senha novamente ou feche a página!")
-        res.innerHTML = ''
-    } else {
-        res.innerHTML = `<h1><a href="./inicio.html" target="_self">Acesse o Álbum de Billy</a></h1>`
-    } 
-}
+// function verificar() {
+//     let senha = document.getElementById("inicio")
+//     let res = document.getElementById("res") 
+//     if (senha.value != "Billy") {
+//         window.alert("Senha incorreta! Digite a senha novamente ou feche a página!")
+//         res.innerHTML = ''
+//     } else {
+//         res.innerHTML = `<h1><a href="./inicio.html" target="_self">Acesse o Álbum de Billy</a></h1>`
+//     } 
+// }
 
 function mudaFoto(foto) {
     document.getElementById("icone").src = foto;
